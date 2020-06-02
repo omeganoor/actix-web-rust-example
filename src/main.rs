c4bef05f52d402a3f046cb675c09631118752d0a
Nur Hakim Soleh <noorhakim.s91@gmail.com>
2020-06-02 13:48:46 +0700

    add services

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+
+use actix_web::{App, HttpServer};
+
+mod config;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    
+    HttpServer::new(|| App::new()
+    .service(config::app::index)
+    .service(config::app::index_path))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
